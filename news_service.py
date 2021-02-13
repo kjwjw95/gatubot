3160c717e479fab6d42ac1dab52d82be846ec8ab
kjwjw95 <jw0905@naver.com>
2021-02-14 02:17:45 +0900

    updates

--- news_service.py
+++ news_service.py
@@ -6,43 +6,48 @@ import json
 def news_kor():
     url = "https://news.naver.com/main/main.nhn?mode=LSD&mid=shm&sid1=101"
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.3'}
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.3"}
     html = requests.get(url, headers=headers,)
-    soup = BeautifulSoup(html.text, 'html.parser')
-    metadata = soup.find_all('div', class_='cluster_text', limit=30)
+    soup = BeautifulSoup(html.text, "html.parser")
+    metadata = soup.find_all("div", class_="cluster_text", limit=30)
     news = []
+    key = 0
     for i in metadata:
-        new = {'title': i.a.get_text(), 'href': i.a.get('href')}
+        new = {"title": i.a.get_text().rstrip(
+            '\n'), "href": i.a.get("href"), "key": key}
+        key += 1
         news.append(new)
-    return json.dumps(news, ensure_ascii=False)
+    return news
 
 
 def news_usa():
-    url = 'https://www.cnbc.com/world/'
-    trans_url = 'https://dapi.kakao.com/v2/translation/translate'
-    KEY = 'KakaoAK 49ba514a295f31e38cc9ceee74c0a862'
-    header_trans = {'Authorization': KEY}
+    url = "https://www.cnbc.com/world/"
+    trans_url = "https://dapi.kakao.com/v2/translation/translate"
+    KEY = "KakaoAK 49ba514a295f31e38cc9ceee74c0a862"
+    header_trans = {"Authorization": KEY}
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.3'}
+        "User-Agent": "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.3"}
     html = requests.get(url, headers=headers)
-    soup = BeautifulSoup(html.text, 'html.parser')
-    metadata = soup.find_all('div', class_='LatestNews-headline', limit=30)
+    soup = BeautifulSoup(html.text, "html.parser")
+    metadata = soup.find_all("div", class_="LatestNews-headline", limit=10)
     news = []
+    key = 0
     for i in metadata:
         title = i.a.get_text()
-        title = title.replace('\n', '')
-        tr = {'query': title, 'src_lang': 'en', 'target_lang': 'kr'}
+        title = title.replace("\n", "")
+        tr = {"query": title, "src_lang": "en", "target_lang": "kr"}
         translate = requests.get(
             url=trans_url, headers=header_trans, params=tr)
-        new = {'kor_title': json.loads(translate.text)[
-            'translated_text'][0][0], 'href': i.a.get('href'), 'eng_title': title}
+        new = {"title": json.loads(translate.text)[
+            "translated_text"][0][0], "href": i.a.get("href"), "eng_title": title, "key": key}
+        key += 1
         news.append(new)
-    return json.dumps(news, ensure_ascii=False)
+    return news
 
 
 def getnews(lng):
     print(lng)
-    if lng == 'kor':
+    if lng == "kor":
         return news_kor()
     else:
         return news_usa()
--- stock_service.py
+++ stock_service.py
@@ -3,7 +3,7 @@ from bson.json_util import dumps
 import json
 import pandas as pd
 import pandas_datareader as pdr
-
+from datetime import date, timedelta
 url = 'mongodb+srv://kjw95:wjddnr1212@cluster0.i61ze.mongodb.net/gatuant?retryWrites=true&w=majority'
 client = pymongo.MongoClient(url)
 db = client.gatuant
@@ -11,27 +11,41 @@ stock_db = db["stock_info"]
 list_db = db["stock"]
 
 
+def Per_cal(eps, stock):
+    return stock / eps
+
+
+def Pbr_cal(bps, stock):
+    return stock/bps
+
+
 def stock_cal(code):
     # code = get_code(code_df, name)
-    try:
-        df = pdr.get_data_yahoo(code, adjust_price=True)
-        dates = date.today()
+    df = pdr.get_data_yahoo(code, adjust_price=True)
+    for ddate in range(0, 5, 1):
+        dates = date.today() - timedelta(ddate)
         dates = dates.strftime('%Y-%m-%d')
-        stocks = int(df.loc[dates, 'Close'])
-        return stocks
-    except:
-        return 0
+        try:
+            stocks = int(df.loc[dates, 'Close'])
+            # print(stocks, " ", dates)
+            if (stocks > 0):
+                break
+        except:
+            continue
+    return stocks
 # 검색한 주식의 정보를 불러오는 메소드
 
 
 def get_stock_info(code):
-    return dumps(stock_db.find_one({'code': code}), ensure_ascii=False)
+    return str(stock_db.find_one({'code': code}))
 
 # 주식 리스트를 불러오는 메소드
 
 
 def get_stock_list():
-    return dumps(list_db.find(), ensure_ascii=False)
+    # print(type(json.dumps(list(list_db.find()))))
+
+    return str(list(list_db.find()))
 
 # 주식 정렬 리스트를 불러오는 메소드
 
@@ -42,12 +56,19 @@ def get_stock_sort(roe, eps, debt):
     d = float(debt)
     query = {'roe.2020.third_quarter': {'$gt': r}, 'roe.2020.second_quarter': {'$gt': r}, 'roe.2020.first_quarter': {'$gt': r},
              'eps_increase.2018': {'$gt': e}, 'eps_increase.2019': {'$gt': e}, 'eps_increase.2020': {'$gt': e}, 'debt': {'$lt': d}}
-    # result = stock_db.find(query)
-    # print(query)
-    # for i in result:
-    #     print(i)
-    # print(r, ' ', e, ' ', d)
-    return dumps(stock_db.find(query), ensure_ascii=False)
+    lists = stock_db.find(query)
+    result = []
+    for l in lists:
+        cd = l['code']
+        share = stock_cal(cd)
+        stock = {"name": l['name'], "share": share, "rim": l['rim'], "per":  str(Per_cal(
+            l['eps']['2020']['third_quarter'], share)), "pbr": str(Pbr_cal(l['bps']['2020']['third_quarter'], share)), "roe": str(l['roe']['2020']['third_quarter']), "debt": str(l['debt']), "code": cd}
+        result.append(stock)
+        # print(query)
+        # for i in result:
+        #     print(i)
+        # print(r, ' ', e, ' ', d)
+    return result
 
 # 주식 추천 리스트를 불러오는 메소드
 
@@ -57,12 +78,11 @@ def get_stock_recommend():
         '$lt': 100.0}, 'profit.2020.third_quarter': {'$gt': 0}, 'profit.2020.first_quarter': {'$gt': 0}, 'profit.2020.second_quarter': {'$gt': 0}}
     lists = stock_db.find(query)
     result = []
-    # for l in lists:
-    #     try:
-    #         cd = l['code']
-    #         stock = stock_cal(cd)
-    #         if l['rim'] > stock:
-    #             result.append(l)
-    #     except:
-    #         continue
-    return dumps(lists, ensure_ascii=False)
+    for l in lists:
+        cd = l['code']
+        stock = stock_cal(cd)
+        if l['rim'] > stock:
+            result.append(l)
+    # print(result)
+    # print(type(result))
+    return str(result)
